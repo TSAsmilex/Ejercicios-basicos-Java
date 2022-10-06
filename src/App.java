90f7a0e0deb0d8e334512667188ed294efeec3a9
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-06 09:47:58 +0200

    Ejercicio 4

--- src/App.java
+++ src/App.java
@@ -5,24 +5,29 @@ import org.w3c.dom.ranges.Range;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ejercicio 1: \n");
+        System.out.println("# Ejercicio 1");
         System.out.println("Anna: "  + ejercicio1("Anna"));
         System.out.println("anna: "  + ejercicio1("anna"));
         System.out.println("Level: " + ejercicio1("level"));
         System.out.println("Nope: "  + ejercicio1("Nope"));
 
-        System.out.println("Ejercicio 2: \n");
-        System.out.println("1: " + ejercicio2(1));
-        System.out.println("11: " + ejercicio2(11));
+        System.out.println("\n# Ejercicio 2");
+        System.out.println("1: "   + ejercicio2(1));
+        System.out.println("11: "  + ejercicio2(11));
         System.out.println("123: " + ejercicio2(123));
 
-        System.out.print("Ejercicio 3\n");
-        System.out.println("0! = " + ejercicio3(0));
-        System.out.println("1! = " + ejercicio3(1));
-        System.out.println("2! = " + ejercicio3(2));
-        System.out.println("3! = " + ejercicio3(3));
+        System.out.println("\n# Ejercicio 3");
+        System.out.println("0! = "  + ejercicio3(0));
+        System.out.println("1! = "  + ejercicio3(1));
+        System.out.println("2! = "  + ejercicio3(2));
+        System.out.println("3! = "  + ejercicio3(3));
         System.out.println("10! = " + ejercicio3(10));
         System.out.println("40! = " + ejercicio3(40));
+
+        System.out.println("\n# Ejercicio 4");
+        System.out.println("mcd(2, 3) = " + ejercicio4(2, 3));
+        System.out.println("mcd(2, 3) = " + ejercicio4(1, 3));
+        System.out.println("mcd(2, 4) = " + ejercicio4(2, 4));
     }
 
     
@@ -56,8 +61,35 @@ public class App {
     }
 
 
-    public static void ejercicio4() {
-
+    public static long ejercicio4(long u, long v) {
+        long t, k;
+ 
+        if (v == 0) return u;
+        
+        u = Math.abs(u);
+        v = Math.abs(v); 
+        if (u < v){
+          t = u;
+          u = v;
+          v = t;
+        }
+       
+        for(k = 1; (u & 1) == 0 && (v & 1) == 0; k <<= 1){
+          u >>= 1; v >>= 1;
+        }
+       
+        t = (u & 1) != 0 ? -v : u;
+        while (t != 0){
+          while ((t & 1) == 0) t >>= 1;
+       
+          if (t > 0)
+            u = t;
+          else
+            v = -t;
+       
+          t = u - v;
+        }
+        return u * k;
     }
 
 
